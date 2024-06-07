22d75ff4610ab90f23598f71be84c0207ac0d8af
chlangus <angusangus11@gmail.com>
2024-06-07 18:32:39 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,10 @@
 function solution(array, commands) {
     var answer = [];
     // commands의 1번째 2번째 인덱스로 자른 다음 3번째 인덱스의 숫자의 수 가져오기
-    for(const command of commands){
+    commands.forEach(command =>
+                     {
         answer.push(array.slice(command[0]-1, command[1]).sort((a,b) => a-b)[command[2]-1]);
-    }
+    });
+    
     return answer;
 }
