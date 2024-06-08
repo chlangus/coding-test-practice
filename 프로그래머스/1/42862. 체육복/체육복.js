1ea012a899f7b94b5f22ec60a1f15cb2853eedb4
chlangus <angusangus11@gmail.com>
2024-06-08 11:41:20 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,16 +1,14 @@
 function solution(n, lost, reserve) {
-     const reserveSet = new Set(reserve.filter(r => !lost.includes(r)));
-  const lostSet = new Set(lost.filter(l => !reserve.includes(l)));
-
-  // 체육복을 빌려줄 수 있는 경우 처리
-  for (let student of Array.from(reserveSet).sort((a, b) => a - b)) {
-    if (lostSet.has(student - 1)) {
-      lostSet.delete(student - 1);
-    } else if (lostSet.has(student + 1)) {
-      lostSet.delete(student + 1);
-    }
-  }
-
-  // 전체 학생 수에서 체육복을 빌리지 못한 학생 수를 빼줌
-  return n - lostSet.size;
+    // 잃어버린애가 체육복을 갖고있을때는 어디에도 없는것과 동일
+    const lostSet = new Set(lost.filter((lostStudent) => !reserve.includes(lostStudent)));
+    const reserveSet = new Set(reserve.filter((reserveStudent) => !lost.includes(reserveStudent)));
+    
+    // 체육복 빌려주는 경우
+    Array.from(reserveSet).sort((a, b) => a - b).forEach(reserveStudent =>{
+        if(lostSet.has(reserveStudent-1)) lostSet.delete(reserveStudent-1);
+        else if(lostSet.has(reserveStudent+1)) lostSet.delete(reserveStudent+1);
+    })
+    
+    // 잃어버린애들 빼주기
+    return n - lostSet.size
 }
