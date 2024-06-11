ad211d138472a7918ce49e12e98edab9bae14881
chlangus <angusangus11@gmail.com>
2024-06-11 16:54:14 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,49 +1,46 @@
 function solution(numbers, hand) {
-     const keyPad = {
-        1: [0, 0], 2: [0, 1], 3: [0, 2],
-        4: [1, 0], 5: [1, 1], 6: [1, 2],
-        7: [2, 0], 8: [2, 1], 9: [2, 2],
-        '*': [3, 0], 0: [3, 1], '#': [3, 2]
+    let answer = '';
+    const keypad = {'1': [0,0], '2': [0,1], '3': [0,2],
+                    '4': [1,0], '5': [1,1], '6': [1,2],
+                    '7': [2,0], '8': [2,1], '9': [2,2],
+                    '*': [3,0], '0': [3,1], '#': [3,2]};
+    
+    let leftCurrent = '*';
+    let rightCurrent = '#';
+    
+    function getDistance(current, target) {
+        return Math.abs(current[0]-target[0]) + Math.abs(current[1]-target[1]);
     };
-
-    function getDistance(pos1, pos2) {
-        return Math.abs(pos1[0] - pos2[0]) + Math.abs(pos1[1] - pos2[1]);
-    }
-
-    let leftThumb = keyPad['*'];
-    let rightThumb = keyPad['#'];
-    let result = '';
-
-    for (let number of numbers) {
-        let target = keyPad[number];
-
-        if (number === 1 || number === 4 || number === 7) {
-            result += 'L';
-            leftThumb = target;
-        } else if (number === 3 || number === 6 || number === 9) {
-            result += 'R';
-            rightThumb = target;
-        } else {
-            let leftDistance = getDistance(leftThumb, target);
-            let rightDistance = getDistance(rightThumb, target);
-
-            if (leftDistance < rightDistance) {
-                result += 'L';
-                leftThumb = target;
-            } else if (rightDistance < leftDistance) {
-                result += 'R';
-                rightThumb = target;
-            } else {
-                if (hand === "right") {
-                    result += 'R';
-                    rightThumb = target;
-                } else {
-                    result += 'L';
-                    leftThumb = target;
+    
+    for(const number of numbers){ 
+        switch(number){
+            case 1: case 4: case 7: 
+                answer+='L';
+                leftCurrent = number;
+                break;
+            case 3: case 6: case 9: 
+                answer+='R';
+                rightCurrent = number;
+                break;
+            case 2: case 5: case 8: case 0: 
+                if(getDistance(keypad[leftCurrent], keypad[number]) < getDistance(keypad[rightCurrent], keypad[number])) {
+                    answer += 'L';
+                    leftCurrent = number;
                 }
-            }
+                else if(getDistance(keypad[leftCurrent], keypad[number]) > getDistance(keypad[rightCurrent], keypad[number])) {
+                    answer += 'R';
+                    rightCurrent = number;
+                }else {
+                    if(hand==='right') {
+                        answer += 'R';
+                        rightCurrent = number;
+                    }else {
+                        answer += 'L';
+                        leftCurrent = number;
+                    }
+                }
         }
     }
-
-    return result;
+    
+    return answer;
 }
