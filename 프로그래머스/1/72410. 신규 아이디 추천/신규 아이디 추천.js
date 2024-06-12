52fd8e1f4775704a0b951cda5cc13c08e7f07121
chlangus <angusangus11@gmail.com>
2024-06-13 00:10:48 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.53 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -3,20 +3,21 @@ function solution(new_id) {
     const lower_id = new_id.toLowerCase(); // 1
     for(let i=0; i<lower_id.length; i++){
         if((lower_id[i] >= '0' && lower_id[i] <= '9') || (lower_id[i] >= 'a' && lower_id[i] <= 'z') || lower_id[i] === '-' || lower_id[i] === '_' || lower_id[i] === '.') { // 2
-            if(answer.length === 0 && lower_id[i] === '.') continue; // 
-            if(lower_id[i] === '.' && answer[answer.length-1] === '.') continue; 
+            if(lower_id[i] === '.' && answer[answer.length-1] === '.') continue; // 3 
+            if(answer.length === 0 && lower_id[i] === '.') continue; // 4
             answer += lower_id[i];
             
         }
     }
-    if(answer === '') return 'aaa';
+    if(answer === '') answer = 'a'; // 5
     
-    if(answer.length > 15) answer = answer.slice(0,15);
-    if(answer[answer.length-1] === '.') answer = answer.slice(0, answer.length-1);
+    if(answer.length > 15) answer = answer.slice(0,15); // 6
+    if(answer[answer.length-1] === '.') answer = answer.slice(0, answer.length-1); // 6
 
-    while(answer.length<=2){
+    while(answer.length<=2){ // 7
         console.log(answer.length, answer);
         answer += answer[answer.length-1];
     }
+    
     return answer;
 }
