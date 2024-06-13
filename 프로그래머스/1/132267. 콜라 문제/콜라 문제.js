50c2f526aca3d49d6ef7ef3758e572c1b519c1b4
chlangus <angusangus11@gmail.com>
2024-06-13 11:36:35 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -4,8 +4,13 @@ function solution(a, b, n) {
     while(a<=restBottle){
         let getBottle = 0;
         
+        // 교환받은 콜라
         getBottle += Math.floor(restBottle/a) * b; 
+        
+        // 교환반은 콜라와 남아있는 콜라 나누고 남은것
         restBottle = getBottle + restBottle % a;
+        
+        // 교환받은 콜라 누산
         answer += getBottle;
     }
     
