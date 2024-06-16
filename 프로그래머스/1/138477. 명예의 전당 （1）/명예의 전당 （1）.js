d920f875abb227e3399ef4c77b0e6d807fd6423d
chlangus <angusangus11@gmail.com>
2024-06-16 12:47:14 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,11 +2,15 @@ function solution(k, score) {
     var answer = [];
     let honor = [];
     for(let i=0; i < score.length; i++){
+        // 명예의전당이 꽉 차있고 최소값이 스코어보다 작을때 숫자 제외
         if(honor.length === k && honor[0] < score[i]) honor.shift();
+        // 명예의 전당 안차있을때 넣어주고 정렬
         if(honor.length < k){
             honor.push(score[i]);
             honor.sort((a,b) => a-b);
         }
+        
+        // 명예의 전당 최솟값 push
         answer.push(honor[0]);
     }
     return answer;
