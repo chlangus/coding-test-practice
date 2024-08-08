804dd36e799278f5bf0ac72de328ef87f7cd24f2
chlangus <angusangus11@gmail.com>
2024-08-08 19:10:02 +0900

    [level 0] Title: 문자열 섞기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,10 +1,9 @@
 function solution(str1, str2) {
     var answer = '';
-    let long_str = str1.length > str2.length ? str1 : str2;
     
-    for(index in long_str){
-        if(str1[index])answer += str1[index];
-        if(str2[index]) answer += str2[index];
+    
+    for(index in str1){
+        answer += str1[index] + str2[index];
     }
     return answer;
 }
