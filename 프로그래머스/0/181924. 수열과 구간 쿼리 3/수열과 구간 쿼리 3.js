8d6f3020e19c04761a3fe55ade9b40aabe2b499a
chlangus <angusangus11@gmail.com>
2024-08-13 14:50:06 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.62 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,10 +1,7 @@
 function solution(arr, queries) {
     var answer = arr;
-    queries.forEach((item)=>{
-        let [first,second] = item;
-        let temp = answer[first];
-        answer[first] = answer[second];
-        answer[second] = temp;
+    queries.forEach(([first, second])=>{
+        [answer[first], answer[second]] = [answer[second], answer[first]];
     })
     return answer;
 }
