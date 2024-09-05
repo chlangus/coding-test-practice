5223f7ff3bbe4e5c5b9764a50d74605695ea877f
chlangus <angusangus11@gmail.com>
2024-09-05 11:48:24 +0900

    [Silver III] Title: 주유소, Time: 216 ms, Memory: 22028 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,14 @@
+const [N, ...input] = require('fs').readFileSync('dev/stdin').toString().trim().split('\n').map(item => item.split(' ').map(item=>+item)); 
+
+let min = input[1][0];
+let answer = 0;
+for (let i = 0; i < N - 1; i++) {
+  if (min > input[1][i]) min = input[1][i];
+  answer += min * input[0][i];
+}
+
+console.log(answer);
+
+// 주유값 적은거 저장해두고 다음꺼보다 작으면 그걸로 거리만큼 더 크면 다음꺼 저장한 후 계속
+// 거리
+// 주유값
